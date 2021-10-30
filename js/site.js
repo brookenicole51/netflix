d2fee2be47c324da2840d2765be82b4d14312194
mhab1700 <mhabrial@csumb.edu>
2021-10-30 13:01:32 -0400

    we

--- js/site.js
+++ js/site.js
@@ -1,3 +1,7 @@
-$(function(){
-  $("#nav-placeholder").load("nav.html");
-});
+//@Brooke Haven, Matthew Lencioni, Carlos Salazar, MAtthew Habrial
+//November 10th 2021
+
+//Use later on for final Nav Bar
+//$(function(){
+  //$("#nav-placeholder").load("nav.html");
+//});
