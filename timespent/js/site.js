c5be1cf323168f24cbea3a5f96904d065d69d2af
mhab1700 <mhabrial@csumb.edu>
2021-12-12 18:42:26 -0500

    and under the sea

--- timespent/js/site.js
+++ timespent/js/site.js
@@ -192,7 +192,7 @@ ScrollReveal().reveal('div');
 //Bar Chart hrs and age
 var data = [
   {
-    x: ['18+', '18-34', '35-49', '50-64', '65+', 'Average Total'],
+    x: ['18 and under', '18-34', '35-49', '50-64', '65+', 'Average Total'],
     y: [4.2, 2, 3.4, 5.4, 6.6, 2],
     type: 'bar',
     marker: {
