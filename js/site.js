435f5d8678c0f7a48c9db9f024413c1a281aca18
mlencioni1 <78108780+mlencioni1@users.noreply.github.com>
2021-12-13 08:32:59 -0800

    final update v1

--- js/site.js
+++ js/site.js
@@ -99,10 +99,62 @@ btnRight.addEventListener("click", (e) => {
     updateIndicators(activeIndex);
   }
 });
+// TIME SPENT QUIZ
+// QUESTION 1
+//Input Box 0-24
+//  timeSpent.value = var num
+//
 
-$("#butt1").click(function(){
-  window.location.href='https://brookenicole51.github.io/netflix/timespent/index.html';
+//QUESTION 2
+//  buttons for interacttivity
+//    don't affect output
+
+$("#yep").click(function(){
+  $("#yes").hide();
+  $("#no").hide();
+  $("#nope").hide();
+  $("#yep").attr('disable', true);
 });
-$("#butt2").click(function(){
-  window.location.href='https://brookenicole51.github.io/netflix/timespent/index.html';
+$("#yes").click(function(){
+  $("#yep").hide();
+  $("#nope").hide();
+  $("#no").hide();
+  $("#yes").attr('disable', true);
+});
+$("#no").click(function(){
+  $("#yep").hide();
+  $("#yes").hide();
+  $("#nope").hide();
+  $("#no").attr('disable', true);
+});
+$("#nope").click(function(){
+  $("#yes").hide();
+  $("#yep").hide();
+  $("#no").hide();
+  $("#nope").attr('disable', true);
+});
+// ----FINAL OUTPUT----
+//I wanted to calculate the percentage difference to the average timeSpent watching netflix
+//  Positive % difference
+//    var pos = ((x2 - x1) / x1)100
+//  Negative % difference
+//    var neg = ((x1 - x2) / x1)100
+//Let x2 = timeSpent
+//Let x1 = Avg. Time Spent = 3.2
+var avg = 3.2;
+
+$("#final").click(function(){
+  $("#output").empty();
+  var num = $("#timeSpent").val();
+  console.log(num);
+
+  if (num < avg) {
+    let posDif = ((avg - num ) / num) * 100
+  $("#output").append("On average you spend " + posDif + "% less time on Netflix than most people.");
+} else if (num > avg){
+    let negDif = ((num - avg) / num) * 100
+  $("#output").append("On average you spend " + negDif + "% more time on Netflix than most people.");
+  } else {
+  $("#output").append("You spend the same amount of time as the average person on Netflix.");
+  }
 });
